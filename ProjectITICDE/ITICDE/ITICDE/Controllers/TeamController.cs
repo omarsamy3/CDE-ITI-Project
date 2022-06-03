14157611a51de6ed313447b08587294261b157ee
Omar Samy <omar.samy3333@gmail.com>
2022-06-03 11:09:13 +0200

    Add User and team controllers, modify connection string

---
+++ ProjectITICDE/ITICDE/ITICDE/Controllers/TeamController.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using ITICDE.Data;
+using ITICDE.Models;
+
+namespace ITICDE.Controllers
+{
+    public class TeamController : Controller
+    {
+        private readonly CDEDBContext _context;
+
+        public TeamController(CDEDBContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Team
+        public async Task<IActionResult> Index()
+        {
+            var cDEDBContext = _context.Teams.Include(t => t.CreatorUser).Include(t => t.Project);
+            return View(await cDEDBContext.ToListAsync());
+        }
+
+        // GET: Team/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var team = await _context.Teams
+                .Include(t => t.CreatorUser)
+                .Include(t => t.Project)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (team == null)
+            {
+                return NotFound();
+            }
+
+            return View(team);
+        }
+
+        // GET: Team/Create
+        public IActionResult Create()
+        {
+            ViewData["CreatorUserId"] = new SelectList(_context.Users, "Id", "ConfirmEmail");
+            ViewData["ProjectId"] = new SelectList(_context.Projects, "Id", "Description");
+            return View();
+        }
+
+        // POST: Team/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name,CreatorUserId,ProjectId")] Team team)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(team);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CreatorUserId"] = new SelectList(_context.Users, "Id", "ConfirmEmail", team.CreatorUserId);
+            ViewData["ProjectId"] = new SelectList(_context.Projects, "Id", "Description", team.ProjectId);
+            return View(team);
+        }
+
+        // GET: Team/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var team = await _context.Teams.FindAsync(id);
+            if (team == null)
+            {
+                return NotFound();
+            }
+            ViewData["CreatorUserId"] = new SelectList(_context.Users, "Id", "ConfirmEmail", team.CreatorUserId);
+            ViewData["ProjectId"] = new SelectList(_context.Projects, "Id", "Description", team.ProjectId);
+            return View(team);
+        }
+
+        // POST: Team/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,CreatorUserId,ProjectId")] Team team)
+        {
+            if (id != team.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(team);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!TeamExists(team.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CreatorUserId"] = new SelectList(_context.Users, "Id", "ConfirmEmail", team.CreatorUserId);
+            ViewData["ProjectId"] = new SelectList(_context.Projects, "Id", "Description", team.ProjectId);
+            return View(team);
+        }
+
+        // GET: Team/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var team = await _context.Teams
+                .Include(t => t.CreatorUser)
+                .Include(t => t.Project)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (team == null)
+            {
+                return NotFound();
+            }
+
+            return View(team);
+        }
+
+        // POST: Team/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var team = await _context.Teams.FindAsync(id);
+            _context.Teams.Remove(team);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool TeamExists(int id)
+        {
+            return _context.Teams.Any(e => e.Id == id);
+        }
+    }
+}
